bd09a790563de11ccaccbf12ffcb1ab355300f92
Lunalik <40746746+lunalik2@users.noreply.github.com>
2018-11-26 22:12:40 -0500

    Force off system (for AutoRCM)
    Code from hekate

--- source/main.cpp
+++ source/main.cpp
@@ -5,10 +5,10 @@
 
 int main(int argc, char **argv)
 {
-    gfxInitDefault();
-    consoleInit(NULL);
-    bpcInitialize();
-    bpcRebootSystem();
-    return 0;
+sd_unmount();
+#ifdef MENU_LOGO_ENABLE
+	free(Kc_MENU_LOGO);
+#endif 
+i2c_send_byte(I2C_5, MAX77620_I2C_ADDR, MAX77620_REG_ONOFFCNFG1, MAX77620_ONOFFCNFG1_PWR_OFF);
+return 0;
 }
-
